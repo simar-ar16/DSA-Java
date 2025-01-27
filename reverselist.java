2f6b2ee44cf1a70f731646744ecb4627decd6bc7
Simarpreet Singh <simarpreet2916@gmail.com>
2025-01-27 15:47:25 +0530

    Initial Commit

---
+++ reverselist.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class reverselist {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    static Node insert(Node node,Node head)
+    {
+        if(head == null){
+        return node;
+        }
+        Node temp=head;
+        while(temp.next != null)
+        {
+            temp=temp.next;
+        }
+        temp.next=node;
+        return head;
+    }
+    static void print(Node head)
+    {
+        Node temp=head;
+        while(temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node head=null;
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int data=in.nextInt();
+            Node node=new Node(data);
+            head=insert(node,head);
+        }
+        Node prev=null;
+        Node curr=head;
+        Node next=head.next;
+        while(curr != null)
+        {
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+            if(next!=null)
+            next=next.next;
+        }
+        print(prev);
+        in.close();
+    }
+}
