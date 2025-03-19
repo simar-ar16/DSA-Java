02c4fbe4582aa525d085399fb318b61fa054e1a3
Simarpreet Singh <simarpreet2916@gmail.com>
2025-03-20 00:08:09 +0530

    Solved problem

---
+++ groupanagrams.java
@@ -0,0 +1,34 @@
+//group anagrams. Leetcode 49
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class groupanagrams {
+
+    public static void main(String[] args) {
+        String[] str={"act","pots","tops","cat","stop","hat"};
+        List<List<String>> st=groupAnagrams(str);
+        System.out.println(st);
+        
+    }
+
+        static List<List<String>> groupAnagrams(String[] strs) {
+            HashMap<String,List<String>> ans=new HashMap<>();
+            for(String str:strs)
+            {
+                char[] ch=str.toCharArray();
+                Arrays.sort(ch);
+                String s=new String(ch);
+                if(!ans.containsKey(s))
+                {
+                    ans.put(s,new ArrayList<>());
+                    
+                }
+                ans.get(s).add(str);
+            }
+            ArrayList res=new ArrayList<>(ans.values());
+            return res;
+        }
+    }
